d3ffe9fd72e7472efa1e209c1598f37cad77a1a2
League Student <league@iMac-18.attlocal.net>
2020-03-07 15:56:20 -0800

    Create unbirthday.java

---
+++ src/_02_unbirthday/unbirthday.java
@@ -0,0 +1,19 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+	String answer = JOptionPane.showInputDialog("What is your birthday?");
+	if (answer.equalsIgnoreCase("March 7")) {
+		JOptionPane.showMessageDialog(null, "Happy Birthday");
+	}
+	if (answer.equalsIgnoreCase("August 29")) {
+JOptionPane.showMessageDialog(null, "You are a legend and a god");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "You don't have a good enough birthday");
+	}
+	
+}
+}
