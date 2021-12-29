c64e8e9e55a7b524162bde630cad179adf1faee3
student <student@ip-172-31-59-166.us-west-2.compute.internal>
2021-12-28 19:52:45 -0800

    banana split + badgers

---
+++ src/_12_badgers/baders.java
@@ -0,0 +1,20 @@
+package _12_badgers;
+
+public class baders{
+	public static void main(String[] args) {
+		for (int i = 0; i < 12; i++) {
+			System.out.println("Badger, ");
+		}
+		for (int i = 0; i < 2; i++) {
+			System.out.println("Mushroom ");
+		}
+		for (int i = 0; i < 12; i++) {
+			System.out.println("Badger, ");
+		}
+		for (int i = 0; i < 2; i++) {
+			System.out.println("Mushroom ");
+		}
+		System.out.println("A Snake!");
+	}
+}
+
---
+++ src/_14_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,8 @@
+void setup() {
+  size(600,600);
+}
+void draw() {
+  text("icecream", 300,250);
+  text("icecream", 300, 275);
+  text("banana", 300, 300);
+}
